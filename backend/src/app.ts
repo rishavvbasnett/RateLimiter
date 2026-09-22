5007030ce0643da7dfcd47fed52c0713378cc1ef
rishavvbasnett <your-noreply-email>
2026-09-21 20:39:19 -0400

    WIRE APP WIDE AND LOGIN ROUTE WITH RATE LIMITING
    What's done in this commit?
    - Create rateLimit middlware in shared/middlwares
    - Wire it up in the whole app to cap amount of requests to 300/min
    - Wire rateLimit middlware on POST "/auth/login" and cap to 5/min
    
    What to do afterwards?
    - Learn JWT blocklist and refresh token and use it on this app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,11 +4,21 @@ import cors from "cors";
 import userRouter from "./features/users/users.routes.js";
 import { errorHandler } from "./shared/middlewares/errorHandler.js";
 import authRouter from "./features/auth/auth.routes.js";
+import rateLimit from "./shared/middlewares/rateLimit.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    maxRequests: 300,
+    resetWindowSeconds: 60,
+    feature: "api",
+    identityFn: (req) => req.ip ?? "unknown",
+  }),
+);
 
 app.use("/auth", authRouter);
 app.use("/users", userRouter);
--- backend/src/features/auth/auth.routes.ts
+++ backend/src/features/auth/auth.routes.ts
@@ -1,8 +1,18 @@
 import express from "express";
 import authController from "./auth.controller.js";
+import rateLimit from "../../shared/middlewares/rateLimit.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/login", authController.login);
+authRouter.post(
+  "/login",
+  rateLimit({
+    maxRequests: 5,
+    resetWindowSeconds: 60,
+    feature: "login",
+    identityFn: (req) => req.ip ?? "unknown",
+  }),
+  authController.login,
+);
 
 export default authRouter;
